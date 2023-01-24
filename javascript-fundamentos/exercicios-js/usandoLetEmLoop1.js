21341722ed792135e3576ae809a262cee9d05b3c
jonatassx96 <jonatas.sx96@gmail.com>
2023-01-24 19:35:11 -0300

    fundamentos js

---
+++ javascript-fundamentos/exercicios-js/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+console.log("Fim do loop.");
+// console.log(i) A variavel so esta visivel no escopo do bloco que foi declarada. Fora do bloco gera erro.
---
+++ javascript-fundamentos/exercicios-js/usandoVar1.js
@@ -0,0 +1,13 @@
+{
+  {
+    {
+      {
+        var sera = "Será?";
+      }
+    }
+  }
+}
+console.log(sera)
+
+// VAR tem escopo global, fora de uma função
+// VAR declarado dentro de uma função, não tem escopo global
